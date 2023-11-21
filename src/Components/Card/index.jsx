578cb3934d0b2873c2268310b2b0b2298de1c668
LeoCodeDev <alb.mar.cta@gmail.com>
2023-11-21 11:14:01 -0500

    feat: :construction: Adding products using fakestoreapi

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,8 +1,17 @@
+const Card = (props) => {
+  const {product} = props
 
-const Card = () => {
   return (
-    <div>
-        
+    <div className='bg-white w-56 h-60 cursor-pointer rounded-lg'>
+        <figure className='relative mb-2 w-full h-4/5'>
+          <span className='absolute bg-white/60 rounded-lg px-3 bottom-0 left-0 m-2 text-sm'>{product.category}</span>
+          <img className='w-full h-full object-cover rounded-lg' src={product.image} alt={product.title} />
+          <div className='absolute top-0 right-0 flex justify-center items-center bg-white/60 w-6 h-6 rounded-full text-md m-2'>+</div>
+        </figure>
+        <p className="flex justify-between items-center">
+          <span className='text-sm font-light'>{product.title}</span>
+          <span className='text-md font-medium'>{`$${product.price}`}</span>
+        </p>
     </div>
   )
 }
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,11 +1,43 @@
 import { Layout } from '../../Components/Layout'
+import { Card } from '../../Components/Card'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import axios from 'axios'
 
 function Home() {
+  const [products, setProducts] = useState([])
+
+  const fetchData = async () => {
+    const { data } = await axios('/products')
+
+    if (!data) return 'No products founds'
+    if (data) setProducts(data)
+  }
+
+  useEffect(() => {
+    try {
+      fetchData()
+    } catch (error) {
+      console.log(error);
+    }
+
+    // fetch('https://fakestoreapi.com/products')
+    // .then(response => response.json())
+    // .then(data => setProducts(data))
+  }, [])
+
   return (
     <Layout>
       Home
+      <div className='grid gap-5 grid-cols-4 w-full max-w-screen-lg'>
+      {
+        products?.map((product)=> (
+          <Card key={product.id} product={product}/>
+        ))
+      }
+      </div>
     </Layout>
   )
 }
 
-export {Home}
+export { Home }
