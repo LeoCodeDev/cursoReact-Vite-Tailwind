59f027924832261908eaf59dbca2c8aef08ce163
LeoCodeDev <alb.mar.cta@gmail.com>
2023-11-24 11:39:52 -0500

    feat: :sparkles: Product Cards on my order component or checkout menu

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -1,6 +1,7 @@
-import {XMarkIcon} from '@heroicons/react/24/outline'
+import { XMarkIcon } from '@heroicons/react/24/outline'
 import { Context } from '../../Context'
 import { useContext } from 'react'
+import { OrderCard } from '../OrderCard'
 
 const CheckoutSideMenu = () => {
   const context = useContext(Context)
@@ -9,16 +10,24 @@ const CheckoutSideMenu = () => {
     context.setProductToShow({})
   }
   return (
-    <aside 
-        className="flex flex-col fixed right-0 border border-black bg-gray-200 rounded-lg w-[360px] h-[calc(100vh-80px)]"
-    >
-        <div className="flex justify-between items-center p-6">
-            <h2 className="font-medium text-xl">My Order</h2>
-            <XMarkIcon 
-            className='h-6 w-6 cursor-pointer'
-            onClick={handleCloseMenu}
-            />
-        </div>
+    <aside className="flex flex-col fixed right-0 border border-black bg-gray-200 rounded-lg w-[360px] h-[calc(100vh-80px)]">
+      <div className="flex justify-between items-center p-6">
+        <h2 className="font-medium text-xl">My Order</h2>
+        <XMarkIcon
+          className="h-6 w-6 cursor-pointer"
+          onClick={handleCloseMenu}
+        />
+      </div>
+      <div >
+        {context.cartProducts.map((product) => (
+          <OrderCard
+            key={product.id}
+            title={product.title}
+            price={product.price}
+            img={product.image}
+          />
+        ))}
+      </div>
     </aside>
   )
 }
