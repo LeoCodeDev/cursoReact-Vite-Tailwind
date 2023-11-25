71ca31ef336b10df02c439aa3b94df89959e4172
LeoCodeDev <alb.mar.cta@gmail.com>
2023-11-25 09:28:33 -0500

    feat: :sparkles: Adding auxiliar function (totalPrice) to calculate the total price of a new order
    this function was created as an auxiliar function --> src/utils/index.js

---
+++ src/utils/index.js
@@ -0,0 +1,15 @@
+/**
+ * This function calculates the total price of a new order
+ * @param {Array} products cartProducts: Array of objects
+ * @returns {number} Total Price
+ */
+
+const totalPrice = (products) => {
+  const total = products.reduce((acc, item) => {
+    return acc + item.price
+  })
+
+  return total
+}
+
+export { totalPrice }
