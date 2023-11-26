dd10d9cbe66f761d57bbb0f7c72813538dbc32a2
LeoCodeDev <alb.mar.cta@gmail.com>
2023-11-25 19:23:05 -0500

    feat: :sparkles: Orders Card created and related components were adapted
    Order Cards were created to show the diferents orders user have

--- src/Pages/MyOrder/index.jsx
+++ src/Pages/MyOrder/index.jsx
@@ -2,15 +2,24 @@ import { useContext } from 'react'
 import { Layout } from '../../Components/Layout'
 import { OrderCard } from '../../Components/OrderCard'
 import { Context } from '../../Context'
+import { ChevronLeftIcon } from '@heroicons/react/24/outline'
+import { Link, useParams } from 'react-router-dom'
 
 function MyOrder() {
   const context = useContext(Context)
-  console.log(context.order);
+  const {id} = useParams()
+  const index = Number(id)
+
   return (
     <Layout>
-      My Order
-      <div className="overflow-y-auto scrollbar-hide">
-        {context.order?.at(-1).products.map((product) => (
+      <div className='flex max-w-[380px] w-full items-center relative justify-center'>
+        <Link to={`/my-orders`}>
+        <ChevronLeftIcon className='w-6 absolute left-0 top-0 cursor-pointer'/>
+        </Link>
+        <h1>My Order</h1>
+      </div>
+      <div className=" max-w-[600px] min-w-[380px] w-[60%] mt-6">
+        {context.order?.at(index ? index - 1 : -1)?.products.map((product) => (
           <OrderCard
             key={product.id}
             title={product.title}
@@ -24,4 +33,4 @@ function MyOrder() {
   )
 }
 
-export {MyOrder}
+export { MyOrder }
--- src/utils/index.js
+++ src/utils/index.js
@@ -3,13 +3,29 @@
  * @param {Array} products cartProducts: Array of objects
  * @returns {number} Total Price
  */
-
 const totalPrice = (products) => {
   const total = products.reduce((acc, item) => {
     return acc + item.price
-  },0)
+  }, 0)
 
   return total
 }
 
-export { totalPrice }
+/**
+ * @param {Date} date
+ * @returns {String} Formatted date
+ */
+const formatDate = (date) => {
+  date = new Date(date)
+  const [day, month, year, hour, minutes] = [
+    date.getDate(),
+    date.getMonth() + 1,
+    date.getFullYear(),
+    date.getHours(),
+    date.getMinutes(),
+  ]
+
+  return `${day}/${month}/${year} ${hour}:${minutes}`
+}
+
+export { totalPrice, formatDate }
